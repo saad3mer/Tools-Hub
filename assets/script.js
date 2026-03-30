e0ba654cba578ccf73ea4f8dce0fceb1f61eec82
SAAD <saad3mer@gmail.com>
2026-03-30 22:02:26 +0200

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,31 @@
+const frame = document.getElementById("frame");
+const tabs = document.querySelectorAll(".tab");
+const darkToggle = document.getElementById("darkToggle");
 
+function switchTool(url,el){
+    frame.src = url;
+    tabs.forEach(t=>t.classList.remove("active"));
+    el.classList.add("active");
+}
+
+function toggleDark(){
+    const isDark = document.body.classList.toggle("dark");
+
+    if(isDark){
+        darkToggle.classList.add("dark");
+        darkToggle.textContent = "☀️";
+    } else {
+        darkToggle.classList.remove("dark");
+        darkToggle.textContent = "🌙";
+    }
+
+    if(frame.contentWindow){
+        frame.contentWindow.postMessage({dark:isDark}, '*');
+    }
+}
+
+frame.onload = () => {
+    if(frame.contentWindow){
+        frame.contentWindow.postMessage({dark: document.body.classList.contains('dark')}, '*');
+    }
+};
